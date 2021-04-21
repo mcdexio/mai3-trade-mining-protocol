2477f8474ae4b55645d74d2246c097fb31195239
renpu-mcarlo <renpu@mcarlo.com>
2021-04-21 18:34:50 +0800

    update

--- scripts/s10test.ts
+++ scripts/s10test.ts
@@ -6,24 +6,14 @@ import {
 } from "./utils";
 
 async function main() {
-    const mcb = "0x8B2c4Fa78FBA24e4cbB4B0cA7b06A29130317093"
+    const mcb = "0xfa53fd78b5176b4d772194511cc16c02c7f183f9"
     const pool = "0x3e46dd62b5ce4246e739748c9de043f9f3337bb2"
-    const pool1 = "0x038c982f67c091c77a358eb35ca8d12e830f29ab"
+    const collateral = "0x8b2c4fa78fba24e4cbb4b0ca7b06a29130317093"
     const mining = await createContract("Mining", [mcb])
     console.log(mining.address)
     await mining.setRebateRate(toWei("0.3"))
-    await mining.addMiningPool(pool)
+    await mining.addMiningPool(pool, collateral)
     await mining.setMingingBudget(toWei("2000"))
-    console.log(await mining.getBudget())
-    console.log(await mining.getMCBToken())
-    console.log(await mining.getRebateRate())
-    console.log(await mining.getMiningPools())
-    await mining.addMiningPool(pool1)
-    console.log(await mining.getMiningPools())
-    await mining.delMiningPool(pool1)
-    console.log(await mining.getMiningPools())
-//    await mining.addMiningPool(pool1)
-//    console.log(await mining.getMiningPools())
 }
 
 ethers.getSigners()
