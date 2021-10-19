73975fece2091f20ec3d6213df9a2c26eec3d170
yangzhao <yangzhao@mcarlo.com>
2021-10-19 19:04:55 +0800

    add epoch0 script

---
+++ merkle/epoch0.js
@@ -0,0 +1,42 @@
+const ethers = require('ethers')
+const { MerkleTree } = require('merkletreejs')
+const epoch0Data = require('./epoch0.json')
+
+const Endpoint = "https://arb1.arbitrum.io/rpc"
+const PrivateKey = process.env["PK"]
+const MerkleRedeemAddress = "0x205285d6eef9055779650f6556c3704a5b514271"
+
+
+const hash = (x) => Buffer.from(ethers.utils.keccak256(x).slice(2), 'hex')
+
+const padAccount = (account) => Buffer.from(account.slice(2).padStart(64, '0'), 'hex')
+
+const makeTree = accounts => new MerkleTree(accounts.map(x => padAccount(x)), hash, { sortPairs: true })
+
+const makeLeaf = (user, amount) => ethers.utils.solidityKeccak256(["address", "uint256"], [user, amount])
+
+async function claim() {
+    const provider = new ethers.providers.JsonRpcProvider(Endpoint)
+    const wallet = new ethers.Wallet(PrivateKey, provider)
+}
+
+function generateRoot() {
+    return makeTree(epoch0Data.map(x => makeLeaf(x[0], x[1]))).getRoot().toString('hex')
+}
+
+function generateProof(user, amount) {
+    return makeTree(epoch0Data.map(x => makeLeaf(x[0], x[1]))).getHexProof(makeLeaf(user, amount))
+}
+
+function main() {
+    // for setter
+    const root = generateRoot()
+    console.log(`root => ${"0x" + root}`)
+    // for claimer
+    const proof = generateProof("0x022e3ce4eda264b3e3fef62036c8182ceb88e6ce", "19672585930000000000")
+    console.log(`proof => ${proof}`)
+    // then call contract to claim
+    // MerkleRedeem("0x205285d6eef9055779650f6556c3704a5b514271").claimEpoch(0, amount, proof)
+}
+
+main()
--- scripts/deployRedeem.ts
+++ scripts/deployRedeem.ts
@@ -9,17 +9,18 @@ import {
 async function main() {
     // const testStakeToken = await createContract("TestERC20", ["TTK", "TTK", 18])
     // const testStakeToken = { address: "or the token u like"} 
-    const redeem = await ensureFinished(createContract("MerkleRedeem"))
-    console.log(redeem.address)
+    // const redeem = await ensureFinished(createContract("MerkleRedeem"))
+    // console.log(redeem.address)
 
-    const proxy = await createContract(
-        "TransparentUpgradeableProxy",
-        [redeem.address, "0xd80c8fF02Ac8917891C47559d415aB513B44DCb6", "0x"]  //ProxyAdmin
-    )
-    const final = await ethers.getContractAt("MerkleRedeem", proxy.address)
-    await final.initialize("0x5fe80d2cd054645b9419657d3d10d26391780a7b") // MCB
+    // const proxy = await createContract(
+    //     "TransparentUpgradeableProxy",
+    //     [redeem.address, "0xd80c8fF02Ac8917891C47559d415aB513B44DCb6", "0x"]  //ProxyAdmin
+    // )
+    // const redeem = await ensureFinished(createContract("MerkleRedeem"))
+    const final = await ethers.getContractAt("MerkleRedeem", "0x205285d6eef9055779650f6556c3704a5b514271")
+    // await final.initialize("0x5fe80d2cd054645b9419657d3d10d26391780a7b") // MCB
 
-    const rewardToken = await final.stakeToken()
+    const rewardToken = await final.token()
     console.log(`MCBStaking is deployed at ${final.address}(${rewardToken})`)
 }
 
